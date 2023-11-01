f15c517db33f32a32cf21eda4a186f442262d8fc
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-11-01 21:15:48 +0400

    add route for download image

--- back/routers/art/index.ts
+++ back/routers/art/index.ts
@@ -1,10 +1,18 @@
+import path from "path";
 import { Response, Request, Router } from "express";
 
 const router = Router();
 
-router.get("/", async (r: Request, res: Response) => {
+router.get("/:name", async (req: Request<{ name: string }>, res: Response) => {
   try {
-    // res.json({ levels: LEVELS });
+    const name = req.params.name;
+    const folder = path.resolve(__dirname, "..", "..", "images", name);
+
+    res.download(folder, name, (e) => {
+      if (e) {
+        res.status(500).send({ message: "Could not download the file" + e });
+      }
+    });
   } catch (e) {
     res.status(500).json({ message: "Unexpected server error" });
   }
